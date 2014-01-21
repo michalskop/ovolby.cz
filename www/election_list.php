1f9b513256451e897e32f34948e87147b700f6c9
Michal Skop <michal.skop@kohovolit.eu>
2014-01-22 00:46:55 +0100

    senate elections 2014 (80)

--- www/election_list.php
+++ www/election_list.php
@@ -5,7 +5,7 @@ $elections = array(
   'ep' => array('name' => 'Evropský parlament', 'year' => array(2004,2009)),
   'president' => array('name' => 'Prezident ČR', 'year' => array(2013)),
   'psp' => array('name' => 'Poslanecká sněmovna', 'year' => array(1996,1998,2002,2006,2010,2013)),
-  'senat' => array('name' => 'Senát', 'year' => array('1996-2013')),
+  'senat' => array('name' => 'Senát', 'year' => array('1996-2013',2014)),
   'kraje' => array('name' => 'Kraje', 'year' => array(2000,2004,2008,2012)),
   'obce' => array('name' => 'Obce', 'year' => array(1994,1998,2002,2006,2010,'X')),
   'fs-sl' => array('name' => 'Federální shromáždění - Sněmovna lidu', 'year' => array(1990,1992)),
