99d26261478e77e5de76b5c403faf1e5d641e80f
Michal Skop <michal.skop@kohovolit.eu>
2017-07-27 22:18:26 +0000

    location of smarty

--- www/settings.php
+++ www/settings.php
@@ -1,6 +1,6 @@
 <?php
 //settings
 $settings = array(
-  'smarty_path' => '/usr/local/lib/php/Smarty/libs/Smarty.class.php',
+  'smarty_path' => '/usr/local/lib/php/Smarty/Smarty.class.php',
 );
 ?>
